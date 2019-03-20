54d2d852e11db5c3a3f9a031006c97f892256680
Lanahrone <lanahrone@gmail.com>
2019-03-20 09:38:26 +0100

    Added endpoint for creating users

--- app/core/models.py
+++ app/core/models.py
@@ -26,7 +26,7 @@ class UserManager(BaseUserManager):
 
 
 class User(AbstractBaseUser, PermissionsMixin):
-    """Custom user model that suport using email instead of username"""
+    """Custom user model that support using email instead of username"""
     email = models.EmailField(max_length=255, unique=True)
     name = models.CharField(max_length=255)
     is_active = models.BooleanField(default=True)
